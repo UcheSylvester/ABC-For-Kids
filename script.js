7a9f93fbf24710af92e18ffa45149e337d4e2be7
Uchenna Sylvester Okoro <okorocode@gmail.com>
2019-01-04 12:42:05 +0100

    click functionality and code organisation

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 "use strict";
 
-// let sounds = document.querySelectorAll('audio');
-// console.log(sounds);
 
-window.addEventListener('keydown', function(e) { 
-    // console.log(e);
+// PLAYING SOUND ON KEYPRESS
+function playSoundKeypress(e) {
     let alphabet = document.querySelector(`div[data-key='${e.keyCode}']`),
         sound = document.querySelector(`audio[data-key='${e.keyCode}']`);
     this.console.log(sound);
@@ -13,4 +11,23 @@ window.addEventListener('keydown', function(e) {
     if(!sound) return;
     sound.currentTime = 0;
     sound.play();
+}
+
+window.addEventListener('keydown', playSoundKeypress);
+
+let alphabets = Array.from(document.querySelectorAll('.alphabet'));
+// console.log(alphabets);
+
+function playSoundClick(e) {
+    let key = e.path[1].dataset.key;
+    let sound = document.querySelector(`audio[data-key='${key}']`);
+    console.log(key, sound);
+
+    if(!sound) return;
+    sound.play();
+}
+
+alphabets.forEach(alphabet => {
+    alphabet.addEventListener('click', playSoundClick);
 })
+
